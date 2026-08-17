231535dce83357768422aadc35c571eb2853aef1
Youssef Mahrous <youssefmahrous667@gmail.com>
2026-08-17 12:32:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
+    allowedHosts: ['frontend-production-6e8f.up.railway.app']
   },
 });
